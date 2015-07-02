cf2ba81f3b6c57ef54ce63ba116ad0d2624ec822
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-02 23:37:43 +0900

    sort of working

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-./build/sch.js: ./src/sch.js
+./build/sch.js: ./src/sch.hs
 	hastec -Wall -fno-warn-unused-do-bind -O2 ./src/sch.hs -isrc -o ./build/sch.js
--- src/sch.hs
+++ src/sch.hs
@@ -1,6 +1,34 @@
 import Haste
 import Haste.DOM
 import Haste.Graphics.Canvas
-import System.Random
 
-main = undefined
+import Control.Applicative
+import Data.Complex
+
+sch :: Double -> Complex Double -> Complex Double
+sch α' z = let
+    α = α' :+ 0
+    p = sqrt $ (1 - α) / α
+    in
+        (z + (2*p))**(1-α) * (z - 2/p)**α
+
+schL :: [Double] -> [Complex Double]
+schL = scanl (flip sch) (0:+0)
+
+drawPath :: Canvas -> [Complex Double] -> IO ()
+drawPath canv =
+    renderOnTop canv . translate (500,350) . stroke . path . map complexToPoint
+
+complexToPoint :: Complex Double -> Point
+complexToPoint (x :+ y) = (x,y)
+
+randomWalk :: Double -> Seed -> [Double]
+randomWalk w = scanl (+) 1 . map ((*w ) . fromIntegral . subtract 1 . (*2))
+    . randomRs (0, 1::Int)
+
+main = do
+    let w = 0.02 :: Double
+    Just canv <- getCanvasById "canv0"
+    ps <- schL . map (*0.90).  take 1000 . randomWalk w <$> newSeed
+    print ps
+    drawPath canv  ps
