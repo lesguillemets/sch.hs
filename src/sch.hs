fef0eee8b56469ac8ec959cf07375fcd1baf2ff9
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-03 00:09:15 +0900

    several updates
    * prevent divergence
    * no need to take care of x < 0 (?)
    * randomwalk from 0 (par *1)
    * normalise

--- src/sch.hs
+++ src/sch.hs
@@ -7,35 +7,38 @@ import Data.Complex
 import Data.List
 import Data.Function (on)
 
+
 sch :: Double -> Complex Double -> Complex Double
 sch α' z = let
     α = α' :+ 0
     p = sqrt $ (1 - α) / α
+    th = 0.0001
     in
-        (z + (2*p))**(1-α) * (z - 2/p)**α
+        if abs α' > th && abs (1-α') > th
+            then (z + (2*p))**(1-α) * (z - 2/p)**α
+            else z
 
 schL :: [Double] -> [Complex Double]
 schL = scanl (flip sch) (0:+0)
 
 drawPath :: Canvas -> [Complex Double] -> IO ()
 drawPath canv =
-    renderOnTop canv . translate (500,350) . stroke . path . map complexToPoint
+    renderOnTop canv . translate (0,350) . stroke . path . map complexToPoint
 
 complexToPoint :: Complex Double -> Point
 complexToPoint (x :+ y) = (x,y)
 
 randomWalk :: Double -> Seed -> [Double]
-randomWalk w = scanl (+) 1 . map ((*w ) . fromIntegral . subtract 1 . (*2))
+randomWalk w = scanl (+) 0 . map ((*w ) . fromIntegral . subtract 1 . (*2))
     . randomRs (0, 1::Int)
 
 main = do
-    let w = 2 :: Double
+    let w = 0.0002 :: Double
     Just canv <- getCanvasById "canv0"
     s <- newSeed
-    let ps = schL .  take 100 $ randomWalk w s
+    let rw = randomWalk w s
+        ps = schL $  take 5000 rw
         m = maximumBy (compare `on` magnitude) ps
+        f = map (* (700/m)) ps
     print m
-    let
-        f = map (* (350/m)) ps
-    print f
     drawPath canv f
